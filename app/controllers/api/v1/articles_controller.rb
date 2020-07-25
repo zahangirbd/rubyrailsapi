f4f223ddb3002520cd9db854faaab8a85f860e50
Zahangir Alam <zahangirbd@hotmail.com>
2020-07-25 02:59:49 +0100

    some notes are added.

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,14 +1,20 @@
 module Api
   module V1
     class ArticlesController < ApplicationController
+
+      # GET /articles
       def index
         articles = Article.order('created_at DESC');
         render json: {status: 'SUCCESS', message:'Articles are loaded', data:articles}, status: :ok
       end
+
+      # GET /articles/:id
       def show
         article = Article.find(params[:id]);
         render json: {status: 'SUCCESS', message:'Article is loaded', data:article}, status: :ok
       end
+
+      # POST /articles
       def create
         article = Article.new(article_params);
         if article.save
@@ -17,9 +23,8 @@ module Api
           render json: {status: 'ERROR', message:'Article is not saved', data:article.error}, status: :unprocessable_entity
         end
       end
-      def article_params
-        params.permit(:title, :body)
-      end
+
+      # PUT /articles/:id
       def update
         article = Article.find(params[:id]);
         if article.update_attributes(article_params)
@@ -28,11 +33,20 @@ module Api
           render json: {status: 'ERROR', message:'Article is not updated', data:article.error}, status: :unprocessable_entity
         end
       end
+
+      # DELETE /articles/:id
       def destroy
         article = Article.find(params[:id]);
         article.destroy
         render json: {status: 'SUCCESS', message:'Article is deleted', data:article}, status: :ok
       end
+
+      private
+
+      def article_params
+        params.permit(:title, :body)
+      end
+
     end
   end
 end
