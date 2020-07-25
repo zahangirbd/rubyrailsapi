2bc50183699de79716d96f4e322576e061929a9e
Zahangir Alam <zahangirbd@hotmail.com>
2020-07-25 01:25:35 +0100

    just used logger to print the input of SQL

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,8 @@ class Article < ApplicationRecord
   validates :body, presence: true
 
   def execute_sql_statement(sql)
+    #ActiveSupport::Logger
+    logger.debug "execute_sql_statement: input sql is #{sql}"
     results = ActiveRecord::Base.connection.execute(sql)
 
     if results.present?
