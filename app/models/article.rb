7ff9b37691e3cdc7689cc5e76d0951cc15487d34
Zahangir Alam <zahangirbd@hotmail.com>
2020-07-25 01:15:53 +0100

    1. sql query execution 2. unit testing for article using rake

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,15 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
+
+  def execute_sql_statement(sql)
+    results = ActiveRecord::Base.connection.execute(sql)
+
+    if results.present?
+      return results
+    else
+      return nil
+    end
+  end
+
 end
